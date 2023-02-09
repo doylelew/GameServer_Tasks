1a33a1bc49e5f54fbd31a1d753cd54e0cbf3f9e0
doylelew <69176020+doylelew@users.noreply.github.com>
2023-02-09 10:17:13 -0500

    Game Baseclass creation

--- Game_Managers/Games/Valheim.py
+++ Game_Managers/Games/Valheim.py
@@ -1,25 +1,17 @@
 import os
 
+import GameBaseClass
+
 
 #####################################################################################
 # Configurable variables for your specific setup
 #####################################################################################
 
-variables = {
-    "bat_files": "D:/SteamLibrary/steamapps/common/Valheim dedicated server"    
-}
-
+bat_files = "D:/SteamLibrary/steamapps/common/Valheim dedicated server"    
+valheim_log_syntax = GameBaseClass.LogSyntax("k_ESteamNetworkingConnectionState_Connected", "k_ESteamNetworkingConnectionState_None", "Net scene destroyed")
 
 #####################################################################################
 # manager object for game settings that gets passed to the Main game_manager object
 #####################################################################################
-class ValheimManager:
     
-    def __init__(self, base_path):
-        self.name = "Valheim"
-        self.base_path = base_path
-        self.log_files = os.path.join(base_path, "Logs/Games/Valheim")
-    
-    def StatusCheck(self):
-        pass
         
